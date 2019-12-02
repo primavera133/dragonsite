5fb8c6e388a84502c4a0511515e11600ae34c567
Jonas Myrenås <jonas.myrenas@ica.se>
2019-12-02 15:50:41 +0100

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,58 @@
 let activeEnv =
-  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 console.log(`Using environment config: '${activeEnv}'`)
-require('dotenv').config({
-  path: `.env.${activeEnv}`
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
 })
 
 module.exports = {
   siteMetadata: {
-    title: `Dragonflies of Europe`
+    title: `Dragonflies of Europe`,
   },
   plugins: [
     {
-      resolve: 'gatsby-source-graphql',
+      resolve: "gatsby-source-graphql",
       options: {
-        typeName: 'DFAPI',
-        fieldName: 'dragonflies',
-        url: 'https://dragonsgraphqlapi.jonasmyrenas.now.sh/api',
+        typeName: "DFAPI",
+        fieldName: "dragonflies",
+        url: "https://dragonsgraphqlapi.jonasmyrenas.now.sh/api",
         headers: {
-          authorization: process.env.AUTH_KEY
-        }
-      }
+          authorization: process.env.AUTH_KEY,
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`
-      }
-    }
-  ]
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GA_KEY,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        // pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional optional fields
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "example.com",
+      },
+    },
+  ],
 }
