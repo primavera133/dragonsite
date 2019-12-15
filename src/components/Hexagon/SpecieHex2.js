c061d0879d35da38c4a9eb657caf7c45943b9317
Jonas Myrenås <jonas@myrenas.se>
2019-12-15 13:03:34 +0100

    refactor species page for touch devices

--- src/components/Hexagon/SpecieHex2.js
+++ src/components/Hexagon/SpecieHex2.js
@@ -2,80 +2,6 @@ import React from 'react'
 import styled from '@emotion/styled'
 import getColor from './getColor'
 
-const HexLink = styled.a`
-  display: block;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  color: #fff;
-  overflow: hidden;
-  -webkit-transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
-  -ms-transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
-  transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
-
-  :hover h3 {
-    opacity: 1;
-    transition: 0.8s;
-  }
-  :focus h3 {
-    opacity: 1;
-    transition: 0.8s;
-  }
-  :hover p {
-    opacity: 1;
-    transition: 0.8s;
-  }
-  :focus p {
-    opacity: 1;
-    transition: 0.8s;
-  }
-`
-
-const HexImg = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background-position: center center;
-  background-size: cover;
-  overflow: hidden;
-  -webkit-clip-path: polygon(
-    50% 0%,
-    100% 25%,
-    100% 75%,
-    50% 100%,
-    0% 75%,
-    0% 25%
-  );
-  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
-
-  :before,
-  :after {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    content: '';
-    opacity: 0;
-    transition: opacity 0.5s;
-  }
-  :before {
-    background: rgba(22, 103, 137, 0.3);
-  }
-  :after {
-    background: linear-gradient(
-      to top,
-      transparent,
-      rgba(0, 0, 0, 0.75) 28%,
-      rgba(0, 0, 0, 0.9) 50%,
-      rgba(0, 0, 0, 0.75) 72%,
-      transparent
-    );
-  }
-`
-
 const HexItem = styled.li`
   position: relative;
   visibility: hidden;
@@ -166,10 +92,88 @@ const HexInner = styled.div`
     outline: 1px solid transparent; /* fix for jagged edges in FF on hover transition */
   }
 
+  :hover div {
+    background-image: none !important;
+  }
   :hover div:before,
   :hover div:after,
   :hover a {
     opacity: 1;
+  }
+`
+
+const HexLink = styled.a`
+  display: block;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  color: #fff;
+  overflow: hidden;
+  -webkit-transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
+  -ms-transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
+  transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
+
+  :hover h3 {
+    opacity: 1;
+    transition: 0.8s;
+  }
+  :focus h3 {
+    opacity: 1;
+    transition: 0.8s;
+  }
+  :hover p {
+    opacity: 1;
+    transition: 0.8s;
+  }
+  :focus p {
+    opacity: 1;
+    transition: 0.8s;
+  }
+`
+
+const HexImg = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-position: center center;
+  background-blend-mode: multiply;
+  background-size: cover;
+  overflow: hidden;
+  -webkit-clip-path: polygon(
+    50% 0%,
+    100% 25%,
+    100% 75%,
+    50% 100%,
+    0% 75%,
+    0% 25%
+  );
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+
+  :before,
+  :after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    content: '';
+    opacity: 0;
+    transition: opacity 0.5s;
+  }
+  :before {
+    // background: rgba(22, 103, 137, 0.3);
+  }
+  :after {
+    // background: linear-gradient(
+    //   to top,
+    //   transparent,
+    //   rgba(0, 0, 0, 0.25) 25%,
+    //   rgba(0, 0, 0, 0.6) 50%,
+    //   rgba(0, 0, 0, 0.25) 75%,
+    //   transparent
+    // );
   }
 `
 
@@ -178,17 +182,25 @@ const HexHeader = styled.h3`
   padding: 5%;
   box-sizing: border-box;
   font-weight: 300;
-  opacity: 0;
-  margin: 0;
 
+  opacity: 1;
   color: #fff;
+  text-shadow: 0.6px 0.6px 1.2px #000, 0 0 0.2em #2e6733;
+  margin: 0;
+  @media not all and (hover: none) {
+    color: #000;
+    text-shadow: none;
+    opacity: 0;
+  }
+
   text-align: center;
   bottom: 50%;
   padding-top: 50%;
   font-size: 0.7rem;
   line-height: 1.2;
-  // word-break: keep-all;
-  // hyphens: auto;
+  word-break: keep-all;
+  word-wrap: break-word;
+  hyphens: auto;
   z-index: 1;
 `
 
@@ -199,9 +211,15 @@ const HexText = styled.p`
   box-sizing: border-box;
   font-size: 0.7rem;
   font-weight: 300;
-  opacity: 0;
+  opacity: 0.8;
+  text-shadow: 1px 1px 2px #000, 0 0 0.2em #2e6733;
+  @media not all and (hover: none) {
+    opacity: 0;
+    text-shadow: none;
+    color: #333;
+  }
 
-  color: #f5ce95;
+  color: #fff;
   text-align: center;
   bottom: 50%;
   padding-top: 60%;
@@ -215,11 +233,11 @@ const HexText = styled.p`
 const SpecieHex = ({ scientific_name, localNames, family, genera, images }) => {
   const img = images.all ? images.all[0] : null
   let imgUrl =
-    'https://res.cloudinary.com/dragonflies/image/upload/c_pad,w_128,h_128/v1/dragonflies/libellula-bw_ozhigo.png'
+    'https://res.cloudinary.com/dragonflies/image/upload/c_fill,w_128,h_128/v1/dragonflies/libellula-bw_ozhigo.png'
   if (img) {
     imgUrl = `https://res.cloudinary.com/${
       images.cloud_name
-    }/image/upload/c_pad,w_256,h_256/v1/${img.public_id}`
+    }/image/upload/c_fill,w_256,h_256/v1/${img.public_id}`
   }
   const color = getColor(family)
   // console.log(family, color)
@@ -230,8 +248,7 @@ const SpecieHex = ({ scientific_name, localNames, family, genera, images }) => {
           <HexImg
             style={{
               backgroundImage: `url(${imgUrl})`,
-              backgroundColor: color.bg,
-              backgroundBlendMode: 'multiply'
+              backgroundColor: color.bg
             }}
           >
             <HexHeader>{scientific_name}</HexHeader>
